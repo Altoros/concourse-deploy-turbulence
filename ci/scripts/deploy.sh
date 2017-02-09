cf016be3f6fc90ba5b19e9128900a6c077dea60f
Andres Garcia Fiorini <andres.fiorini@gmail.com>
2017-02-08 22:12:45 -0300

    added a space

--- ci/scripts/deploy.sh
+++ ci/scripts/deploy.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 #
+### Load env
+
+project_dir=$(readlink -f "$(dirname $0)/../..")
+source $project_dir/common/utils/load-bosh-env.sh 
+
 bosh upload-release https://bosh.io/d/github.com/cppforlife/turbulence-release
 
 # vault write secret/turbulence-wdc1-prod-pros turbulence-api-ip
 
 bosh -n -d turbulence deploy ./manifests/turbulence.yml \
-  -v turbulence_api_ip=$TURBULENCE_API_IP \
-  -v director_ip=$DIRECTOR_IP \
-  --var-file director_ssl_ca=$DIRECTOR_SSL_CA \
-  -v director_client=$DIRECTOR_CLIENT \
-  -v director_client_secret=$DIRECTOR_CLIENT_SECRET \
-  --vars-store ./creds.yml
+  -v turbulence_api_ip=$(vault read --field=broker-ip $VAULT_HASH_PROPS  )$\
+  -v director_ip=$BOSH_DIRECTOR_IP \
+  --var-file director_ssl_ca=$BOSH_DIRECTOR_SSL_CA \
+  -v director_client=$BOSH_DIRECTOR_CLIENT \
+  -v director_client_secret=$BOSH_DIRECTOR_CLIENT_SECRET \
 
+
+#  --vars-store ./creds.yml
 # EOF
