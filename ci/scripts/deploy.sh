fc231644378c62de5b671520c9c9e69d99783478
Andres Garcia Fiorini <andres.fiorini@gmail.com>
2017-02-08 23:14:02 -0300

    changed name of vault value for api-ip

--- ci/scripts/deploy.sh
+++ ci/scripts/deploy.sh
@@ -6,11 +6,11 @@ project_dir=$(readlink -f "$(dirname $0)/../..")
 source $project_dir/common/utils/load-bosh-env.sh 
 
 bosh upload-release https://bosh.io/d/github.com/cppforlife/turbulence-release
-
+export VAULT_HASH_PROPS=secret/bosh-$FOUNDATION_NAME-pros
 # vault write secret/turbulence-wdc1-prod-pros turbulence-api-ip
 
 bosh -n -d turbulence deploy ./manifests/turbulence.yml \
-  -v turbulence_api_ip=$(vault read --field=broker-ip $VAULT_HASH_PROPS  )$\
+  -v turbulence_api_ip=$(vault read --field=turbulence-api-ip $VAULT_HASH_PROPS)\
   -v director_ip=$BOSH_DIRECTOR_IP \
   --var-file director_ssl_ca=$DIRECTOR_SSL_CA \
   -v director_client=$DIRECTOR_CLIENT \
