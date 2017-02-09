e8e03a42033507fada951b75f8cfd27031a9fd96
Andres Garcia Fiorini <andres.fiorini@gmail.com>
2017-02-08 23:19:25 -0300

    changed dir for manifest

--- ci/scripts/deploy.sh
+++ ci/scripts/deploy.sh
@@ -9,7 +9,7 @@ bosh upload-release https://bosh.io/d/github.com/cppforlife/turbulence-release
 export VAULT_HASH_PROPS=secret/bosh-$FOUNDATION_NAME-pros
 # vault write secret/turbulence-wdc1-prod-pros turbulence-api-ip
 
-bosh -n -d turbulence deploy ./manifests/turbulence.yml \
+bosh -n -d turbulence deploy $project_dir/manifests/turbulence.yml \
   -v turbulence_api_ip=$(vault read --field=turbulence-api-ip $VAULT_HASH_PROPS)\
   -v director_ip=$BOSH_DIRECTOR_IP \
   --var-file director_ssl_ca=$DIRECTOR_SSL_CA \
