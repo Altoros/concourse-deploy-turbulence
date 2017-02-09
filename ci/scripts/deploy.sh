3a4eb9631e88426bff0c09a7a76f728972ebd7cf
Andres Garcia Fiorini <andres.fiorini@gmail.com>
2017-02-09 20:26:00 -0300

    changed values of vault

--- ci/scripts/deploy.sh
+++ ci/scripts/deploy.sh
@@ -32,15 +32,14 @@ API_CERT_PRIVATE_KEY=`echo $JSON | jq -r '.turbulence_api_cert.private_key' `
 API_CERT_CA=`echo $JSON | jq -r  '.turbulence_api_cert.ca' `
 TURBULENCE_API_PASSWORD=`echo $JSON | jq -r '.turbulence_api_password'`
 set -x
-vault write turbulence-$FOUNDATION_NAME-pros/turbulence-api-ca  \
-                            ca=$API_CA \
-                            certificate=$API_CERTIFICATE \
-                            private_key=$API_PRIVATE_KEY 
+vault write turbulence-$FOUNDATION_NAME-pros/turbulence-api-ca  ca.ca="$API_CA" \
+                            ca.certificate="$API_CERTIFICATE" \
+                            ca.private_key="$API_PRIVATE_KEY" 
 
 vault write turbulence-$FOUNDATION_NAME-pros/turbulence-api-cert  \
-                            ca="$API_CERT_CA" \
-                            certificate="$API_CERT_CERTIFICATE" \
-                            private_key="$API_CERT_PRIVATE_KEY" 
+                            cert.ca="$API_CERT_CA" \
+                            cert.certificate="$API_CERT_CERTIFICATE" \
+                            cert.private_key="$API_CERT_PRIVATE_KEY" 
 
 vault write turbulence-$FOUNDATION_NAME-pros/turbulence-api-cert  \
                            turbulence_api_password=$TURBULENCE_API_PASSWORD
